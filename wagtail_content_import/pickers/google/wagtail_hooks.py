cf518c6439329b2923031d701d6c019b225b3df5
Karl Hobley <karl@kaed.uk>
2019-10-10 16:03:59 +0100

    Cleanout unused code

--- wagtail_content_import/pickers/google/wagtail_hooks.py
+++ wagtail_content_import/pickers/google/wagtail_hooks.py
@@ -1,27 +1,14 @@
-from django.urls import include
-
 import json
 
 from django.conf import settings
 from django.shortcuts import render
-from django.urls import path
 from django.utils.text import slugify
 
 from wagtail.admin.action_menu import PageActionMenu
 from wagtail.admin.views.pages import get_valid_next_url_from_request
 from wagtail.core import hooks
 
-from .utils import parse_document, get_oauth_credentials, GooglePicker
-
-from . import urls
-from ...mappers.streamfield import StreamFieldMapper
-
-
-@hooks.register('register_admin_urls')
-def register_admin_urls():
-    return [
-        path('content-import-google/', include(urls, namespace='content_import_google')),
-    ]
+from .utils import parse_document, GooglePicker
 
 
 @hooks.register("before_create_page")
