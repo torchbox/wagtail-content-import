4f57ae518a16b32d5641bbb987e4909c448ac407
zerolab <dan@zerolab.org>
2023-03-28 11:04:36 +0100

    Update google_picker.js to use the new APIs
    - https://developers.google.com/drive/picker/guides/overview
    - https://developers.google.com/drive/picker/reference#docs-view
    - https://developers.google.com/drive/api/quickstart/js

--- wagtail_content_import/pickers/google/static/wagtail_content_import/google_picker.js
+++ wagtail_content_import/pickers/google/static/wagtail_content_import/google_picker.js
@@ -7,106 +7,98 @@
             this.importPageUrl = importPageUrl;
             this.csrfToken = csrfToken;
 
-            this.googleApisLoaded = false;
-            this.showOnReady = false;
-
-            // Load auth and picker APIs
-            gapi.load('client:auth2:picker', () => {
-                gapi.client.init({
-                    apiKey: this.authOptions.pickerApiKey,
-                    clientId: this.authOptions.clientId,
-                    discoveryDocs: ['https://docs.googleapis.com/$discovery/rest?version=v1'],
-                    scope: GOOGLE_API_SCOPE
-                }).then(() => {
-                    this.googleApisLoaded = true;
-
-                    if (this.showOnReady) {
-                        this.show();
-                    }
-                }).catch(error => {
-                    console.error(error);
-                });
-            });
+            this.clientInitialised = false;
+            this.accessToken = null;
         }
 
-        authenticate() {
-            const googleAuth = gapi.auth2.getAuthInstance();
+        picker() {
+            let docsView = new google.picker.DocsView(google.picker.ViewId.DOCS)
+                .setMimeTypes("application/vnd.google-apps.document")
+                .setSelectFolderEnabled(true)
+                .setIncludeFolders(true);
 
-            if (googleAuth.isSignedIn.get()) {
-                return Promise.resolve(googleAuth.currentUser.get().getAuthResponse());
-            }
+            let sharedDrivesView = new google.picker.DocsView(google.picker.ViewId.DOCS)
+                .setMimeTypes("application/vnd.google-apps.document")
+                .setSelectFolderEnabled(true)
+                .setIncludeFolders(true)
+                .setEnableDrives(true);
 
-            return googleAuth.signIn({ scope: GOOGLE_API_SCOPE }).then((result) => {
-                return result.getAuthResponse();
-            });
-        }
-
-        picker(oauthToken, callback) {
-            let docsView = new google.picker.DocsView(google.picker.ViewId.DOCUMENTS);
-            docsView.setMimeTypes("application/vnd.google-apps.document");
-            docsView.setSelectFolderEnabled(true);
-            docsView.setIncludeFolders(true);
-
-
-            let sharedDrivesView = new google.picker.DocsView(google.picker.ViewId.DOCUMENTS);
-            sharedDrivesView.setMimeTypes("application/vnd.google-apps.document");
-            sharedDrivesView.setSelectFolderEnabled(true);
-            sharedDrivesView.setIncludeFolders(true);
-            sharedDrivesView.setEnableDrives(true);
-
-
-            let picker = new google.picker.PickerBuilder()
-                .enableFeature(google.picker.Feature.SUPPORT_DRIVES)
+            const picker = new google.picker.PickerBuilder()
                 .setAppId(this.authOptions.appId)
                 .setDeveloperKey(this.authOptions.pickerApiKey)
-                .setOAuthToken(oauthToken)
+                .setOAuthToken(this.accessToken)
                 .addView(docsView)
                 .addView(sharedDrivesView)
-                .setCallback(callback)
+                .setCallback(this.pickerCallback)
                 .build();
 
             picker.setVisible(true);
         }
 
+        pickerCallback(data) {
+            if (data.action == google.picker.Action.PICKED) {
+                gapi.client.docs.documents.get({
+                    documentId: data.docs[0].id
+                }).then((response) => {
+                    // POST the JSON content of the document to the create page view
+                    // Use a hidden form so the browser reloads with the result of this request
+                    let form = document.createElement('form');
+                    form.action = this.importPageUrl;
+                    form.method = 'POST';
+                    form.style.visibility = 'hidden';
+                    document.body.appendChild(form);
+
+                    let csrfTokenField = document.createElement('input');
+                    csrfTokenField.type = 'hidden';
+                    csrfTokenField.name = 'csrfmiddlewaretoken';
+                    csrfTokenField.value = this.csrfToken;
+                    form.appendChild(csrfTokenField);
+
+                    let googleDocField = document.createElement('input');
+                    googleDocField.type = 'hidden';
+                    googleDocField.name = 'google-doc';
+                    googleDocField.value = response.body;
+                    form.appendChild(googleDocField);
+
+                    form.submit();
+                });
+            }
+        }
+
         show() {
-            if (!this.googleApisLoaded) {
+            if (!window.googlepicker_client_loaded && !window.googlepicker_api_loaded) {
                 console.warn("Google APIs aren't loaded yet");
-                this.showOnReady = true;
                 return;
             }
 
-            this.authenticate()
-                .then(auth => {
-                    return this.picker(auth.access_token, (data) => {
-                        if (data.action == google.picker.Action.PICKED) {
-                            gapi.client.docs.documents.get({
-                                documentId: data.docs[0].id
-                            }).then((response) => {
-                                // POST the JSON content of the document to the create page view
-                                // Use a hidden form so the browser reloads with the result of this request
-                                let form = document.createElement('form');
-                                form.action = this.importPageUrl;
-                                form.method = 'POST';
-                                form.style.visibility = 'hidden';
-                                document.body.appendChild(form);
-
-                                let csrfTokenField = document.createElement('input');
-                                csrfTokenField.type = 'hidden';
-                                csrfTokenField.name = 'csrfmiddlewaretoken';
-                                csrfTokenField.value = this.csrfToken;
-                                form.appendChild(csrfTokenField);
-
-                                let googleDocField = document.createElement('input');
-                                googleDocField.type = 'hidden';
-                                googleDocField.name = 'google-doc';
-                                googleDocField.value = response.body;
-                                form.appendChild(googleDocField);
-
-                                form.submit();
-                            });
-                        }
-                    });
+            if (!this.clientInitialised) {
+                gapi.load('picker', () => {
+                    this.clientInitialised = true;
+                    this.show()
                 });
+            }
+            else {
+                let tokenClient = google.accounts.oauth2.initTokenClient({
+                    client_id: this.authOptions.clientId,
+                    scope: GOOGLE_API_SCOPE,
+                    callback: async (response) => {
+                        if (response.error !== undefined) {
+                            throw (response);
+                        }
+                        this.accessToken = response.access_token;
+                        this.picker();
+                    }
+                });
+
+                if (this.accessToken === null) {
+                    // Prompt the user to select a Google Account and ask for consent to share their data
+                    // when establishing a new session.
+                    tokenClient.requestAccessToken({prompt: 'consent'});
+                } else {
+                    // Skip display of account chooser and consent dialog for an existing session.
+                    tokenClient.requestAccessToken({prompt: ''});
+                }
+            }
         }
     }
 
