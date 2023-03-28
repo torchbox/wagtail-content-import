d984a65277e58f031f99a6d074a0be5c178ecd7b
Joshua Munn <public@elysee-munn.family>
2023-03-28 17:14:14 +0100

    Initialise gapi client, store oauth token in localStorage

--- wagtail_content_import/pickers/google/static/wagtail_content_import/google_picker.js
+++ wagtail_content_import/pickers/google/static/wagtail_content_import/google_picker.js
@@ -8,7 +8,7 @@
             this.csrfToken = csrfToken;
 
             this.clientInitialised = false;
-            this.accessToken = null;
+            this.accessToken = localStorage.getItem("contentImportGoogleAccessToken");
         }
 
         picker() {
@@ -29,40 +29,45 @@
                 .setOAuthToken(this.accessToken)
                 .addView(docsView)
                 .addView(sharedDrivesView)
-                .setCallback(this.pickerCallback)
+                .setCallback(this.getPickerCallBack())
                 .build();
 
             picker.setVisible(true);
         }
 
-        pickerCallback(data) {
-            if (data.action == google.picker.Action.PICKED) {
-                gapi.client.docs.documents.get({
-                    documentId: data.docs[0].id
-                }).then((response) => {
-                    // POST the JSON content of the document to the create page view
-                    // Use a hidden form so the browser reloads with the result of this request
-                    let form = document.createElement('form');
-                    form.action = this.importPageUrl;
-                    form.method = 'POST';
-                    form.style.visibility = 'hidden';
-                    document.body.appendChild(form);
+        getPickerCallBack() {
+            // The callback will be executed with `this' not bound to an instance of this class,
+            // so bind the required members in a closure
+            const { csrfToken, importPageUrl } = this;
+            return (data) => {
+                if (data.action == google.picker.Action.PICKED) {
+                    gapi.client.docs.documents.get({
+                        documentId: data.docs[0].id
+                    }).then((response) => {
+                        // POST the JSON content of the document to the create page view
+                        // Use a hidden form so the browser reloads with the result of this request
+                        let form = document.createElement('form');
+                        form.action = importPageUrl;
+                        form.method = 'POST';
+                        form.style.visibility = 'hidden';
+                        document.body.appendChild(form);
 
-                    let csrfTokenField = document.createElement('input');
-                    csrfTokenField.type = 'hidden';
-                    csrfTokenField.name = 'csrfmiddlewaretoken';
-                    csrfTokenField.value = this.csrfToken;
-                    form.appendChild(csrfTokenField);
+                        let csrfTokenField = document.createElement('input');
+                        csrfTokenField.type = 'hidden';
+                        csrfTokenField.name = 'csrfmiddlewaretoken';
+                        csrfTokenField.value = csrfToken;
+                        form.appendChild(csrfTokenField);
 
-                    let googleDocField = document.createElement('input');
-                    googleDocField.type = 'hidden';
-                    googleDocField.name = 'google-doc';
-                    googleDocField.value = response.body;
-                    form.appendChild(googleDocField);
+                        let googleDocField = document.createElement('input');
+                        googleDocField.type = 'hidden';
+                        googleDocField.name = 'google-doc';
+                        googleDocField.value = response.body;
+                        form.appendChild(googleDocField);
 
-                    form.submit();
-                });
-            }
+                        form.submit();
+                    });
+                }
+            };
         }
 
         show() {
@@ -72,12 +77,15 @@
             }
 
             if (!this.clientInitialised) {
-                gapi.load('picker', () => {
+                gapi.load('client', async () => {
+                    await gapi.client.init({
+                        apiKey: this.authOptions.pickerApiKey,
+                        discoveryDocs: ['https://docs.googleapis.com/$discovery/rest?version=v1'],
+                    });
                     this.clientInitialised = true;
-                    this.show()
+                    gapi.load('picker', () => { this.show() });
                 });
-            }
-            else {
+            } else {
                 let tokenClient = google.accounts.oauth2.initTokenClient({
                     client_id: this.authOptions.clientId,
                     scope: GOOGLE_API_SCOPE,
@@ -85,6 +93,7 @@
                         if (response.error !== undefined) {
                             throw (response);
                         }
+                        localStorage.setItem("contentImportGoogleAccessToken", response.access_token);
                         this.accessToken = response.access_token;
                         this.picker();
                     }
