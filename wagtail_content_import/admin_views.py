45dd2203e0b74e29432cdc04316cfe82397a15e6
Joshua Munn <public@elysee-munn.family>
2023-10-17 17:13:26 +0100

    Use kwargs for render_modal_workflow

--- wagtail_content_import/admin_views.py
+++ wagtail_content_import/admin_views.py
@@ -4,8 +4,8 @@ from wagtail.admin.modal_workflow import render_modal_workflow
 def confirm_dialog(request):
     return render_modal_workflow(
         request,
-        'wagtail_content_import/confirm_dialog.html',
-        None,
-        {},
+        html_template='wagtail_content_import/confirm_dialog.html',
+        js_template=None,
+        template_vars={},
         json_data={'step': 'dialog'}
     )
